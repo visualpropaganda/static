0e734b7a02e72d5c63db23f1aa60c155706d0d6c
Mirko Echghi-Ghamsari <visualpropaganda.01@googlemail.com>
2013-04-29 19:10:04 +0200

    Zwischenstand 2013-04-29

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ $(window).load(function() {
 			$('#mobnav').removeClass('hidden');
 			$('#mainnav').addClass('hidden');
 			$('#mobnav').addClass('active');
-			$('#mobnav>ul ul').siblings('a').addClass('icon');	// icon-angle in Nav-Elements with dropdown-sub-nav-elements
+			$('#mobnav>ul ul').siblings('a').addClass('icon');	// icon-angle in Nav-Elements with dropdown-sub-nav-element
 
 		// Open/Close Navigation + Toggle Nav-Buttons
 			$('.open').on('click', function(e) {
 				e.preventDefault();		//preventing the anchor tags from following any links or changing the address in the address bar
-				$('div.wrapper').addClass('mobilemenu');
-					// .css('position','fixed');
+				$('div.wrapper').addClass('mobilemenu')
+					.css('position','fixed');
 				$(this).css(
 					{'display':'none',
 					'visibility':'hidden'
@@ -32,10 +32,12 @@ $(window).load(function() {
 					'visibility':'visible'
 					});
 				$('#mobnav ul ul').removeClass('mobdropdown');			// Hide all dropdowns
+				$('#mobnav a').removeClass('pressed');
 			});
 			$('.close').on('click', function(e) {
 				e.preventDefault();		//preventing the anchor tags from following any links or changing the address in the address bar
-				$('div.wrapper').removeClass('mobilemenu');
+				$('div.wrapper').removeClass('mobilemenu')
+						.css('position','relative');
 				$(this).css(
 					{'display':'none',
 					'visibility':'hidden'
@@ -46,18 +48,37 @@ $(window).load(function() {
 					});
 			});
 
+			// Close nav by touching partial off screen content
+			$('section').on('click', function(e) {
+				if ($('div.wrapper').hasClass('mobilemenu')) {
+					e.preventDefault();
+					$('div.wrapper').removeClass('mobilemenu')
+						.css('position','relative');
+					$('.close').css(
+						{'display':'none',
+						'visibility':'hidden'
+						});
+					$('.open').css(
+						{'display':'block',
+						'visibility':'visible'
+						});
+				}
+			});
+
+
 		// Dropdown-Elements in Mobile Nav
 
 			// set class 'mobdropdown'
-			$('#mobnav>ul>li a').on('click', function(e) {
+			$('#mobnav>ul>li>a').on('click', function(e) {
 				e.preventDefault();		//preventing the anchor tags from following any links or changing the address in the address bar
+				$('#mobnav>ul>li>a').removeClass('pressed');
 				if ($(this).closest('li').children('ul').hasClass('mobdropdown')) {		//if dropdown already open -> close it
 						$('#mobnav ul ul').removeClass('mobdropdown');
 					}
 				else {
 					$('#mobnav ul ul').removeClass('mobdropdown');	//first remove all 'mobdropdown' classes
-					$(this).parents().children('ul')				//then add class to specific ul
-					.addClass('mobdropdown');
+					$(this).siblings('ul').addClass('mobdropdown');	//then add class to ul on same level
+					$(this).addClass('pressed');
 				}
 			});
 
